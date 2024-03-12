28333baf8a21323cb5a6dcabe2f71eef5d72257d
Jlupuka <dima.usachev.0790@mail.ru>
2024-03-12 18:12:10 +0500

    Тренировки в полёте

--- main.py
+++ main.py
@@ -5,9 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
-def index():
+def index() -> str:
     title = "Миссия Колонизация Марса"
     return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof: str) -> str:
+    image_dict: dict[str: str] = {
+        'Научные симуляторы': 'med.png',
+        'Инженерные тренажеры': 'engineer.png'
+    }
+    typeMission = 'Научные симуляторы'
+    if prof.lower() in {'инженер', 'строитель'}:
+        typeMission = 'Инженерные тренажеры'
+
+    return render_template('training.html', prof=prof, imageName=image_dict[typeMission],
+                           typeMission=typeMission)
 
 
 if __name__ == '__main__':
